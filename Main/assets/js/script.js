6375d1e852930d74d6e318c3301ba964e641fc44
Luka <lukaericnu@gmail.com>
2022-08-17 19:27:40 -0500

    installed API + added elements for display

--- Main/assets/js/script.js
+++ Main/assets/js/script.js
@@ -1,5 +1,12 @@
 var searchCity = document.querySelector('#search-form');
 
+$(document).ready(function () {
+  $('#CCD').hide()
+});
+
+
+var APIKey = "afad52e8ca20da3827e5cb6084687d0f";
+
 function searchCitySubmit(evt) {
   evt.preventDefault();
 
@@ -8,14 +15,14 @@ function searchCitySubmit(evt) {
   if (!searchedCity) {
     console.error('Enter relevant US City');
     return;
-  }
-
+  } else {
   console.log(searchedCity);
 
+  var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchedCity + '&units=imperial' + '&appid=' + APIKey;
+  fetch(requestURL);
+      $("#cityName").text(searchedCity);
   
-
-
-
-}
+      $('#CCD').show();
+}};
 
 searchCity.addEventListener('submit', searchCitySubmit);
